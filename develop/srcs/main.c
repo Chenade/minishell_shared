2a644ac81d11f00c8714f3acee8c28d7917b422c
coisu <cscard1002@gmail.com>
2023-02-18 03:33:26 +0100

    add : function to rm space

--- develop/srcs/main.c
+++ develop/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jischoi <jischoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 16:12:50 by ykuo              #+#    #+#             */
-/*   Updated: 2023/02/18 03:07:15 by jischoi          ###   ########.fr       */
+/*   Updated: 2023/02/18 03:29:46 by jischoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,9 +94,7 @@ void	sigint_handler(int sig)		// need to change exit_code -> 130;
 
 int	is_sep(char s)
 {
-	if (s == '<' || s == '>' || s == '|' || s == ' '
-		|| s == - '<' || s == - '>' || s == - '&'
-		|| s == - '|')
+	if (s == '<' || s == '>' || s == '|' || s == ' ')
 		return (1);
 	return (0);
 }
@@ -115,13 +113,13 @@ void	rm_space_sep(char *cmd)
 	i = 0;
 	while (*cmd)
 	{
-		if ((is_sep(*cmd) && *(cmd - 1) == ' '))
-			++i;
-		else if ((is_sep(*cmd - 1) && *(cmd) == ' '))
-			++i;
+		if ((is_sep(*cmd) && *(cmd - 1) == ' ') && ++i)
+			*(cmd - 1) = *(cmd + i - 1);
+		else if ((*(cmd) == ' ' && is_sep(*(cmd - 1))) && ++i)
+			*(cmd) = *(cmd + i);
 		else
 			cmd++;
-		*cmd = *(cmd + i);
+		*(cmd) = *(cmd + i);
 	}
 }
 
@@ -129,9 +127,11 @@ void	filter_cmd(char *cmd)
 {
 	int	i;
 	char quot;
+	char *tmp;
 
 	i = 0;
 	quot = '\0';
+	tmp = cmd;
 	while (*cmd == ' ')
 		cmd++;
 	while (*cmd)
@@ -144,9 +144,8 @@ void	filter_cmd(char *cmd)
 			cmd++;
 		*cmd = *(cmd + i);
 	}
-	printf("BEFORE RM : %s\n", cmd);
+	cmd = tmp;
 	rm_space_sep(cmd);
-	printf("AFT RM : %s\n", cmd);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -172,7 +171,10 @@ int	main(int argc, char **argv, char **envp)
 			printf("  out : %s\n", out);
 			filter_cmd(out);
 			printf("> out : %s\n", out);
-			check_args(out, &prompt);
+			// printf("BEFORE RM : %s\n", out);
+			// rm_space_sep(out);
+			// printf("AFT RM : %s\n", out);
+			// check_args(out, &prompt);
 			g_sig.exit_status = 0;
 		}
 		free (out);
