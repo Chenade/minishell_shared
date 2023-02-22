ac8a3aebdc4aad8d9d94a7b8cf20b9a14b49730a
coisu <cscard1002@gmail.com>
2023-02-22 21:35:47 +0100

    removed printf lines

--- develop/srcs/temp.c
+++ develop/srcs/temp.c
@@ -7,15 +7,15 @@ void	print_token(t_token *token)
 
 	i = 1;
 	tmp = token;
-	printf("======print_token======\n");
+	// printf("======print_token======\n");
 	while (tmp->str)
 	{
-		printf("%d ) STR : %15s  TYPE : %d  \n", i++, tmp->str, tmp->type);
+		printf("		%d ) STR : %15s  TYPE : %d  \n", i++, tmp->str, tmp->type);
 		if (!tmp->next)
 			break;
 		tmp = tmp->next;
 	}
-	printf("======print_token======\n");
+	// printf("======print_token======\n");
 }
 
 int	print_env(char **envp)
